586cee875b3b3332bd159e9e6a459607bcefaf8e
yuvrajmodz <ys8619050@gmail.com>
2024-11-06 13:32:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def verify_email(email):
         return jsonify({"response": "Email status not found or invalid email."})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5015))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
